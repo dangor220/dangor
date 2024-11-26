8d491d37180fc5b7568e224c223dc79af30b8b8c
Danil Gordeev <dan.gor220@yandex.ru>
2024-11-26 09:04:03 +0300

    feat: remove test prewiew inner text

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -3,7 +3,7 @@ import styles from './Preview.module.scss';
 export default function Preview() {
   return (
     <div className={styles.preview}>
-      <p>Hi, I'm Mr. Wolf</p>
+      <p></p>
     </div>
   );
 }
