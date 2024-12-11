c60d1dc88aa2af65865bc22ecf01fb33aeaba0d8
Danil Gordeev <dan.gor220@yandex.ru>
2024-12-11 17:40:11 +0300

    feat: work styled react-pdf

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo, useRef, useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
+import useResizeObserver from 'use-resize-observer';
 
 import styles from './Popup.module.scss';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
@@ -7,6 +8,8 @@ import 'react-pdf/dist/Page/TextLayer.css';
 import UndoIcon from '@mui/icons-material/Undo';
 import RedoIcon from '@mui/icons-material/Redo';
 import CloseIcon from '@mui/icons-material/Close';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
@@ -21,11 +24,15 @@ type popupData = {
 export default function Popup({ dataFile, setPopup }: popupData): React.ReactNode {
   const [numPages, setNumPages] = useState<number>(0);
   const [pageNumber, setPageNumber] = useState<number>(1);
+  const [isLoad, setIsLoad] = useState<boolean>(false);
   const reader = useRef<HTMLDivElement>(null);
+
+  const { ref, width = 1 } = useResizeObserver<HTMLDivElement>();
 
   const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages);
     setPageNumber(1);
+    setIsLoad(true);
   };
   const changePage = (offset: number) => {
     setPageNumber((prevPageNumber) => prevPageNumber + offset);
@@ -52,25 +59,47 @@ export default function Popup({ dataFile, setPopup }: popupData): React.ReactNod
     [],
   );
 
+  const LoadingPDF = (): React.ReactNode => {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <CircularProgress
+          sx={(theme) => ({
+            color: theme.palette.grey[200],
+            ...theme.applyStyles('dark', {
+              color: theme.palette.grey[800],
+            }),
+          })}
+        />
+      </Box>
+    );
+  };
+
   return (
     <div className={styles.popup} onClick={handleClickOutside}>
       <div className={styles.reader} ref={reader}>
-        <Document file={dataFile} options={options} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber} />
-        </Document>
-        <div className={styles.menu}>
-          <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
-            <UndoIcon />
-          </button>
-          <p>
-            {pageNumber || (numPages ? 1 : '--')} / {numPages || '--'}
-          </p>
-          <button type="button" disabled={pageNumber >= numPages} onClick={nextPage}>
-            <RedoIcon />
-          </button>
-        </div>{' '}
-        <div className={styles.close} onClick={handleClosePopup}>
-          <CloseIcon />
+        <div className={styles.viewer} ref={ref}>
+          <Document
+            loading={<LoadingPDF />}
+            noData={<LoadingPDF />}
+            file={dataFile}
+            options={options}
+            onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} width={width || undefined} loading={<LoadingPDF />} />
+          </Document>
+          <div className={isLoad ? styles.menu : styles.disabled}>
+            <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
+              <UndoIcon />
+            </button>
+            <p>
+              {pageNumber || (numPages ? 1 : '--')} / {numPages || '--'}
+            </p>
+            <button type="button" disabled={pageNumber >= numPages} onClick={nextPage}>
+              <RedoIcon />
+            </button>
+          </div>{' '}
+          <div className={isLoad ? styles.close : styles.disabled} onClick={handleClosePopup}>
+            <CloseIcon />
+          </div>
         </div>
       </div>
     </div>
