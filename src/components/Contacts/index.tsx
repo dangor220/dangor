d0538a1ea9b5a37a43682ff6835ac4ce1e816749
Danil Gordeev <dan.gor220@yandex.ru>
2025-01-14 23:35:35 +0300

    feat: create contact form component

--- src/components/Contacts/index.tsx
+++ src/components/Contacts/index.tsx
@@ -3,5 +3,9 @@ import React from 'react';
 import styles from './Contacts.module.scss';
 
 export default function Contacts(): React.ReactNode {
-  return <div className={`${styles.contacts} container`} id="contacts" data-anchor></div>;
+  return (
+    <div className={`${styles.contacts}`} id="contacts" data-anchor>
+      <div className={`${styles.wrapper} container`}></div>
+    </div>
+  );
 }
