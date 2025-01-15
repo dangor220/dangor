7173de844a0c394681eb4f4086ba5ec77290a1d4
Danil Gordeev <dan.gor220@yandex.ru>
2025-01-15 23:21:19 +0300

    feat: create submit handle for contact form

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,5 +1,82 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export default function ContactForm(): React.ReactNode {
-  return <></>;
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const [t] = useTranslation();
+
+  const apiOpenKey = import.meta.env.VITE_API_KEY;
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    if (!formRef.current) return;
+
+    const formData = new FormData(formRef.current);
+    const url = `https://formhub.dev/io/${apiOpenKey}`;
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const result = await response.text();
+        if (result === 'OK') {
+          console.log('Message was send');
+        } else {
+          console.log('Error: ' + result);
+        }
+      } else {
+        console.error('Error: ', response.status, response.statusText);
+      }
+    } catch (error) {
+      console.error('Network error:', error);
+    }
+  };
+  return (
+    <form id="contact-form" ref={formRef} onSubmit={handleSubmit}>
+      <div className="user-data">
+        <input
+          type="text"
+          name="name"
+          placeholder={t('placeholderName')}
+          value={user}
+          onChange={(e) => {
+            setUser(e.target.value);
+          }}
+        />
+        <input
+          type="email"
+          name="email"
+          value={email}
+          placeholder={t('placeholderEmail')}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </div>
+
+      <div className="user-message">
+        <input
+          type="text"
+          name="message"
+          value={message}
+          placeholder={t('placeholderMessage')}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+      </div>
+      <input type="hidden" name="redirect-to" value="no-redirect"></input>
+      <button type="submit" id="submit-button">
+        {t('submitForm')}
+      </button>
+    </form>
+  );
 }
--- src/components/Contacts/index.tsx
+++ src/components/Contacts/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import styles from './Contacts.module.scss';
+import ContactForm from '../ContactForm';
 
 export default function Contacts(): React.ReactNode {
   return (
     <div className={`${styles.contacts}`} id="contacts" data-anchor>
-      <div className={`${styles.wrapper} container`}></div>
+      <div className={`${styles.wrapper} container`}>
+        <ContactForm />
+      </div>
     </div>
   );
 }
