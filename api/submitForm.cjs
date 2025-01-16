835d23b8802053ac2fdfacd5a1569c5c93cf8a3a
Danil Gordeev <dan.gor220@yandex.ru>
2025-01-16 23:37:03 +0300

    feat: try set api on vercel

--- api/submitForm.cjs
+++ api/submitForm.cjs
@@ -1,6 +1,10 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+const fetch = require('node-fetch');
 
-export default async function submitForm(req: VercelRequest, res: VercelResponse) {
+/**
+ * @param {import('@vercel/node').VercelRequest} req
+ * @param {import('@vercel/node').VercelResponse} res
+ */
+module.exports = async function submitForm(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).send('Method Not Allowed');
   }
@@ -14,7 +18,7 @@ export default async function submitForm(req: VercelRequest, res: VercelResponse
   try {
     const { formData } = req.body;
 
-    console.log('API_KEY:', process.env.API_KEY);
+    console.log('API_KEY:', API_KEY);
 
     const response = await fetch(`https://formhub.dev/io/${API_KEY}`, {
       method: 'POST',
@@ -35,4 +39,4 @@ export default async function submitForm(req: VercelRequest, res: VercelResponse
     console.error('Error:', error);
     res.status(500).send('Internal Server Error');
   }
-}
+};
